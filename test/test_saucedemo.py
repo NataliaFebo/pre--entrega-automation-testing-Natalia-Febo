d5e2b4ded7499b1018e3a545514360856bcd6193
nati_loly@hotmail.com <nati_loly@hotmail.com>
2025-10-07 17:34:03 -0300

    primer mapero de entregables

---
+++ test/test_saucedemo.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+@pytest.fixture
+def driver():
+    # configuracion para consultar a selenium web driver
+
+
+def test_login():
+    # logeo de usuario con username y password
+    # click al boton de login
+    # redireccion a la pagina de inventario
+    # verificar titulo de la pagina (ventanita)
+
+
+def test_catalogo():
+    # logeo de usuario con username y password
+    # click al boton de login
+
+    # verificar el titulo del htlm
+
+    # verificar que existan productos visibles en la pagina (len())
+    # verificar elementos importantes de la pagina
+
+
+
+
+def test_carrito():
+    # logeo de usuario con username y password
+    # click al boton de login
+    # redirecciona la pagina de carrito de compras
+    # incremento del carrito al agregar un producto
+    # verificar que se visualice el producto correcto en el carrito
